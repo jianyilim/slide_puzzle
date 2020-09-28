7e6035c104807322678e4af335a63fb191770ca9
jianyilim <68362506+jianyilim@users.noreply.github.com>
2020-09-28 08:45:47 +0800

    gh
    c

--- lib/src/list_details/character.dart
+++ lib/src/list_details/character.dart
@@ -30,12 +30,27 @@ class Character {
 
 //source: https://www.giantbomb.com/dragon-ball-z/3025-159/characters/
 final characters = <Character>[
+    Character(
+    title: "Kenali Zaman Prasejarah\n认识史前时代",
+    description:
+    "Prasejarah ialah zaman manusia belum mengenal tulisan. Manusia tidak mempunyai tulisan untuk mencatat dan merekodkan kegiatan kehidupan mereka. Kewujudan manusia prasejarah dapat diketahui melalui kajian arkeologi dengan menggunakan kaedah gali cari. Prasejarah dibahagikan kepada empat Zaman utama iaitu Paleolitik, Mesolitik, Neolitik dan Logam.",
+    avatar: "images/characters/kenali1.png",
+    color: 0xFF355070,
+    image2: "images/characters/kenali2.jpg",
+    description2: '认识史前时代史前时代是史前在未有文字记載事情之前的时期，人们没有文字记录日常生活和活动，称为“史前时代”。虽然没有文字记载，但是通过考古挖掘，可以让我们知道人类的历史和文化。史前时代可以分为四个阶段：旧石器时代，中石器时代，新石器时代和金属器时代。',
+    image3: "images/characters/kenali3.jpg",
+    description3: 'Zaman Prasejarah merangkumi zaman sebelum adanya bukti bertulis. Pengetahuan mengenai sejarah silam ini bergantung pada penyelidikan arkeologi yang dibuat secara sistematik dan analisis bersifat saintifik. Terdapat tiga (3) tahap perkembangan tamadun yang jelas pada zaman ini, iaitu: 1. Aktiviti mengumpul dan berburu. 2. Aktiviti pertanian awal dan penternakan haiwan. 3. Aktiviti perlombongan, teknologi logam dan perdagangan awal.',
+    image4: "images/characters/kenali4.jpg",
+    description4: '',
+    image5: "images/characters/kenali5.jpg",
+    description5: '',
+  ),
   Character(
     title: "ZAMAN PALEOLITIK\n旧石器时代",
     description:
         "ZAMAN MESOLITIK (14,000 SM)\n\nTempoh peralihan dari Paleolitik ke Neolitik dikenali sebagai Mesolitik. Ia mempunyai ciri-ciri dari Zaman Paleolitik dan Zaman Neolitik. Pada zaman Mesolitik, penerus manusia Paleolitik menyesuaikan diri dengan pemanasan global yang pesat.",
     avatar: "images/characters/vegeta.png",
-    color: 0xFF238BD0,
+    color: 0xFF6d597a,
     image2: "images/characters/paleolitik2.jpg",
     description2: '\n\nKEGIATAN SOSIAL, EKONOMI MASYARAKAT PALEOLITIK\n\nMasyarakat zaman Paleolitik hidup secara berpindah randah. Manusia pada zaman ini berusaha menyesuaikan kehidupan dengan alam sekeliling mereka. Faktor ini penting kerana alam sekeliling merupakan sumber utama kepada mereka mendapatkan makanan. Manusia pada Zaman Paleolitik tinggal di dalam dan di sekitar gua yang berhampiran. Mereka sudah tahu menggunakan api untuk memanaskan badan dan melindungi diri daripada binatang buas.\nManusia Zaman Paleolitik dikenali sebagai Pengumpul. Mereka mengumpulkan hasil hutan contohnya sayur-sayuran, buah-buahan liar, siput, cendawan dan sebagainya untuk memenuhi kehidupan harian. Sekiranya mereka menjumpai haiwan yang mati, mereka akan memakan dagingnya. Kemudian, mereka belajar memburu. Mereka memburu binatang besar seperti burung, mommath, rusa, beruang dan lain-lain lagi.',
     image3: "images/characters/paleolitik3.jpg",
@@ -50,7 +65,7 @@ final characters = <Character>[
     description:
         "ZAMAN MESOLITIK (14,000 SM)\n\nTempoh peralihan dari Paleolitik ke Neolitik dikenali sebagai Mesolitik. Ia mempunyai ciri-ciri dari Zaman Paleolitik dan Zaman Neolitik. Pada zaman Mesolitik, penerus manusia Paleolitik menyesuaikan diri dengan pemanasan global yang pesat.",
     avatar: "images/characters/meso.png",
-    image2: "images/characters/meso2.png",
+    image2: "images/characters/meso2.jpg",
     description2: "\n\nKEGIATAN SOSIAL, EKONOMI MASYARAKAT MESOLITIK\n\nGaya hidup Masyarakat Zaman Mesolitik tinggal dalam kelompok secara berpindah-randah. Manusia pada zaman ini tinggal di dalam gua, di persisiran pantai dan di tepi sungai.",
     image3: "images/characters/meso3.jpg",
     description3: "\n\nKehidupan Masyarakat pada zaman ini sangat bergantung pada memancing dan memburu kerana mereka menetap di tepi pantai dan sungai pada masa tertentu. Oleh itu, mereka dapat menangkap ikan dengan mudah.\n\nMasyarakat Mesolitik mengusahakan batu kepada alat kecil yang digunakan untuk pelbagai perkara. Alat baru dikembangkan. Kapak yang dipegang di tangan pada zaman Paleolitik kini disangkut ke pegangan kayu. Mereka juga diberi permukaan yang halus. Serpihan kecil batu api tajam yang dikenal sebagai mikrolit digunakan sebagai kepala anak panah dan mata lembing Oleh itu, muncul alat penangkapan ikan.",
@@ -58,7 +73,7 @@ final characters = <Character>[
     description4: "\n\n中石器时代 （公元前14000年）\n\n从旧石器时代过渡到新石器时代的时期被称为中石器时代。它具有旧石器时代和新石器时代的特征。在中石器时代，旧石器时代的人类的后代适应了迅速的全球变暖。",
     image5: "images/characters/meso5.jpg",
     description5: "\n\n史前时代的社会, 经济活动\n\n中石器时代的人类生活方式也是小规模的游牧。这时代的人类生活在山洞，河口和海岸。这个时代的生活在主要依靠于捕鱼和打猎因为他们生活在河口和海岸，在那里他们可以轻松钓到鱼。\n\n中石器时代的特点是细石器的制作。人们发明了新的工具。旧石器时代手中的斧头现在已连接到木柄上。它们还具有光滑的表面。尖锐的小碎片称为微晶石，被用作箭头和矛尖。这样就出现了渔具。",
-    color: 0xFF354C6C,
+    color: 0xFFb56576,
   ),
   Character(
     title: "ZAMAN NEOLITIK\n新石器时代",
@@ -74,10 +89,10 @@ final characters = <Character>[
         image4: "images/characters/neolitik4.jpg",
         description4:
         "\n\n新石器时代（公元前3000年）\n\n大约在公元前3000年左右，新石器时代革命开始发展。新石器时代是石器时代的最后一部分。它标志着人类历史上从小型游牧民族向大型农业定居点和早期文明的转变。",
-        image5: "images/characters/neolitik5.jpeg",
+        image5: "images/characters/neolitik5.jpg",
         description5:
         "\n\n史前时代的社会, 经济活动\n\n在新石器时代，人们开始大规模的固定群居取代了游牧生活方式。这个时代的人类开始实践社会制度。这意味着一群人住在同一地区。人类从穴居转为巢居，开始修建房子。他们用木头, 甘草等制成房子。许多的房屋建在一起成了一个小的部落。部落通常位于河边或海边。促使新石器时代人们定居生活的主要因素是，他们必须防止自己的农作物和牲畜受到动物和害虫的破坏。这时代的第一个转变是人们寻获了农业和畜牧业。他们从采集者和狩猎者转变自己的食品生产者。各种动植物被饲养并繁殖了以供人类食用。新石器时代文化的是种植玉米，谷物，南瓜和其他农作物。他们还开始饲养牲畜，如狗，猪，牛和山羊。另外，随着人口逐渐的增加，这个时代的人已经擅长分工与合作。通常，男人会去打猎，而女人会照料牲畜等等。不仅在农业方面，而且在工具，房屋建筑和艺术（包括陶器，缝纫和编织）方面也取得了进步。通过新的定居生活方式，人们能够将时间和精力集中在开发有助于促进新贸易的工具上。为了使农业更容易，新石器时代的人们开始创造出更好的工具，以帮助他们更好地生长，耕种和收割庄稼。这也带来了犁的诞生。这史代的特征是用各种形式的光滑的石器工具，例如小斧头和斧头。据说他们具有适用于制造设备的岩石和土壤类型的技能和知识。另一个发明是用于存放食物和补给品的陶瓷锅。锅最初是由动物石块或干肠制成的。新石器时代也生产了编织工具，使人们开始用自然和牲畜生产的材料制造纺织品和服装。新石器时代在基础设施领域取得了巨大的发展。他们引入了车轮。轮子帮助人们来回运输重物。除了制造轮子之外，还制造了小船。小船的发明使他们即使隔在远处也很容易彼此交流。交易活动已在新石器时代开始发生。他们在村庄之间进行交易。他们交换日常所需的物品。",
-    color: 0xFF6F2B62,
+    color: 0xFFe56b6f,
   ),
   Character(
     title: "ZAMAN LOGAM\n金属器时代",
@@ -96,6 +111,6 @@ final characters = <Character>[
         image5: "images/characters/logam5.jpg",
         description5:
         '\n\n史前时代的社会, 经济活动\n\n在金属时代，科技发生了变化，但人类还是维持新石器时代的生活方式，不过群居规模扩大了。与新石器时代相比，他们建造的房屋更好，更坚固。城墙也被人们建立为了保护村庄免受敌人的攻击或野生动物袭击。此外，他们有首领作为领导。在金属时代，人类知道如何开始使用铜，锡，青铜和铁等金属来制造工具和武器。他们也知道如何在热炉中开采，加热金属以生产工艺工具。这时生产的工具更锋利，更耐用。如：铃铛，镰刀，飞镖等。他们还经常使用黄金和白银等贵重金属制作珠宝。通过所有这些变化，人们的生活变得更加轻松。乡村和城市也变得更大，更繁荣。这个时代延续了新石器时代的经济体系。人类改变了农业和畜牧业的生活方式，他们从收集自己的粮食到生产粮食。值得一提的是，采矿，手工业和贸易也在扩大。从沿海地区开始的贸易活动后来扩展到偏远社区。所有的史前时代不信仰任何宗教，但是他们实践万物有灵的信念。在金属时代，宗教也蓬勃发展。这个时代的人们有一定的信仰和生活方式。他们奉行风俗习惯制度化仪式感。他们还用石块进行了葬礼。他们也擅长制作船形棺材。此外，在语言和文字方面也可以看到发展。这个时代的艺术倾向于使用具象征性的符号代表文字。目的是记录行政，宗教，经济等事项。在这里，我们可以看见各史代有着明显的进步，那就是人类为了改善更加美好和复杂的生活所取得的进步。',
-    color: 0xFF447C12,
+    color: 0xFFeaac8b,
   ),
 ];
